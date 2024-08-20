3255f3e47cb48f9a2c7e39afc92ec5be63ebe37f
lain <aballard@student.neumont.edu>
2024-08-20 11:17:07 -0600

    CouchDb

--- MongoData/src/main/java/org/example/Main.java
+++ MongoData/src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import org.example.controller.CouchController;
 import org.example.controller.MenuController;
 import org.example.controller.NeoController;
 
 public class Main {
     public static void main(String[] args){
-        new MenuController().run();
+        //new MenuController().run();
+        new CouchController();
         System.out.println("END PROGRAM");
     }
 }
